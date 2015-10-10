1a31d96686fc76c4ec82cedd34c9f7041a1670c4
Kristiina Sikkar <ksikkar@klass1-01-l.intra.itcollege.ee>
2015-10-10 17:21:33 +0300

    tunnitöö 10.10.2015

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -17,5 +19,43 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor = new Foor();
+
+        int count = 0;
+        while (count < 10) {
+            foor.vahetaPunast();
+            foor.paus(3.0);
+            foor.vahetaKollast();
+            foor.paus(0.7);
+            foor.kustutaPunane();
+            foor.kustutaKollane();
+            foor.paus(0.2);
+            foor.vahetaRohelist();
+            foor.paus(3.0);
+            foor.kustutaRoheline();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.kustutaRoheline();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.kustutaRoheline();
+            foor.vahetaKollast();
+            foor.paus(1.0);
+            foor.kustutaKollane();
+            foor.paus(0.2);
+            foor.vahetaPunast();
+
+            count = count+1;
+            //System.out.println("Count = " + count);
+        }
+    }
 }
