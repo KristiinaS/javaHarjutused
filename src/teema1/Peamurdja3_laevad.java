a793c54bd51800088787e7f37b81529e712935d7
Kristiina Sikkar <ksikkar@klass1-05-l.intra.itcollege.ee>
2015-10-11 16:19:42 +0300

    laevade pommitamine Level 1

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -1,7 +1,10 @@
 package teema1;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 /**
  * NB! Lahenda käesolev ülesanne konsoolis. Pole vaja JavaFXi siia segada.
@@ -16,4 +19,49 @@ import java.util.Scanner;
  *    nii vertikaalselt kui ka horisontaalselt?
  */
 public class Peamurdja3_laevad {
-}
+    public static void main(String[] args) {
+
+        //int[] laud = {1, 0, 0, 1, 1, 0, 1, 1, 0};
+
+
+        int[]laud = new int[9];
+        for (int i=0;i<9; i++){
+            int n = (int)(Math.random()*2);
+            laud[i]=n;
+            //System.out.println(laud[i]);
+        }
+
+        System.out.println(Arrays.toString(laud));
+
+        int sum = IntStream.of(laud).sum();
+        //System.out.println("Laevu on järel  " + sum);
+        System.out.println("Laevu on järel  " + sum);
+
+        while (sum > 0) {
+
+            Scanner kasutaja = new Scanner(System.in);
+            System.out.println("Sistesta number 0-st 8-ni");
+            int sisestus = kasutaja.nextInt();
+            System.out.println(laud[sisestus]);
+
+
+            if (laud[sisestus] == 1) {
+                System.out.println("Pihtas!");
+                laud[sisestus] = 0;
+                //System.out.println(Arrays.toString(laud));
+                int sum2 = IntStream.of(laud).sum();
+                System.out.println("---------------");
+                System.out.println("Laevu on järel  " + sum2);
+                sum = sum-1;
+            } else if (laud[sisestus] == 0) {
+                    System.out.println("Läks mööda. Proovi uuesti.");
+                    int sum2 = IntStream.of(laud).sum();
+                    System.out.println("---------------");
+                    System.out.println("Laevu on järel  " + sum2);
+            }
+
+            }
+        System.out.println("Palju õnne! Võitsid mängu!");
+        }
+    }
+
